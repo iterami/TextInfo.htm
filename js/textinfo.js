7f31fec775e35bccab84583362e9c59933be46ae
honzi <czechinthemail@gmail.com>
2017-01-20 18:30:36 -0800

    moved event bindings to onload

--- js/textinfo.js
+++ js/textinfo.js
@@ -38,46 +38,6 @@ function generate_list(){
     document.getElementById('key-list').innerHTML = temp.join(' ');
 }
 
-document.getElementById('text').oninput = function(){
-    // Fetch lowercase value.
-    var value = this.value.toLowerCase();
-
-    // Display length of inputted text.
-    document.getElementById('length').innerHTML = value.length;
-
-    // Reset characters.
-    document.getElementById('characters').innerHTML = '';
-    var characters = '0123456789abcdefghijklmnopqrstuvwxyz !@#$%^&*()_-+={}|:;"\'<,>.?/'.split('');
-    var output = '';
-
-    // Count how many times each character appears in inputted text.
-    for(var character in characters){
-        var character_count = value.replace(
-          new RegExp(
-            '[^' + characters[character] + ']',
-            'g'// Global
-          ),
-          ''
-        ).length;
-
-        // Only display characters that appear in inputted text.
-        if(character_count <= 0){
-            continue;
-        }
-
-        output +=
-          '<tr><td>'
-          + characters[character]
-          + '</td><td>'
-          + character_count
-          + '</td><td>'
-          + ((character_count / value.length) * 100).toFixed(2)
-          + '%</td></tr>';
-    }
-
-    document.getElementById('characters').innerHTML = output;
-};
-
 window.onload = function(e){
     settings_init({
       'prefix': 'KeyInfo.htm-',
@@ -95,4 +55,44 @@ window.onload = function(e){
     document.getElementById('fetch-keyinfo-keycode').onclick = fetch_keyinfo_keycode;
     document.getElementById('generate').onclick = generate_list;
     document.getElementById('settings-reset').onclick = settings_reset;
+
+    document.getElementById('text').oninput = function(){
+        // Fetch lowercase value.
+        var value = this.value.toLowerCase();
+
+        // Display length of inputted text.
+        document.getElementById('length').innerHTML = value.length;
+
+        // Reset characters.
+        document.getElementById('characters').innerHTML = '';
+        var characters = '0123456789abcdefghijklmnopqrstuvwxyz !@#$%^&*()_-+={}|:;"\'<,>.?/'.split('');
+        var output = '';
+
+        // Count how many times each character appears in inputted text.
+        for(var character in characters){
+            var character_count = value.replace(
+              new RegExp(
+                '[^' + characters[character] + ']',
+                'g'// Global
+              ),
+              ''
+            ).length;
+
+            // Only display characters that appear in inputted text.
+            if(character_count <= 0){
+                continue;
+            }
+
+            output +=
+              '<tr><td>'
+              + characters[character]
+              + '</td><td>'
+              + character_count
+              + '</td><td>'
+              + ((character_count / value.length) * 100).toFixed(2)
+              + '%</td></tr>';
+        }
+
+        document.getElementById('characters').innerHTML = output;
+    };
 };
