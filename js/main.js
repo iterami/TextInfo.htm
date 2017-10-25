3fda2cfe2453e856eee924d1d29a9f03e71c8f1d
honzi <czechinthemail@gmail.com>
2017-10-24 23:52:39 -0700

    moved event binds to events arg

--- js/main.js
+++ js/main.js
@@ -12,6 +12,51 @@ function repo_init(){
         'generate': {
           'onclick': generate_list,
         },
+        'text': {
+          'oninput': function(){
+              // Fetch lowercase value.
+              var value = this.value.toLowerCase();
+
+              // Display length of inputted text.
+              document.getElementById('length').innerHTML = value.length;
+
+              // Reset characters.
+              var characters = '0123456789abcdefghijklmnopqrstuvwxyz !@#$%^&*()_-+={}|:;"\'<,>.?/'.split('');
+              var output = '';
+
+              // Count how many times each character appears in inputted text.
+              for(var character in characters){
+                  var character_count = value.replace(
+                    new RegExp(
+                      '[^' + characters[character] + ']',
+                      'g'// Global
+                    ),
+                    ''
+                  ).length;
+
+                  // Only display characters that appear in inputted text.
+                  if(character_count <= 0){
+                      continue;
+                  }
+
+                  output +=
+                    '<tr><td>'
+                    + characters[character]
+                    + '</td><td>'
+                    + character_count
+                    + '</td><td>'
+                    + ((character_count / value.length) * 100).toFixed(7)
+                    + '%</td></tr>';
+              }
+
+              core_html_modify({
+                'id': 'characters',
+                'properties': {
+                  'innerHTML': output,
+                },
+              });
+          },
+        },
       },
       'storage': {
         'base-keycode': 0,
@@ -19,50 +64,6 @@ function repo_init(){
         'keycode': 72,
         'keycode-range': 1000,
       },
-      'title': 'KeyInfo.htm',
+      'title': 'TextInfo.htm',
     });
-
-    document.getElementById('text').oninput = function(){
-        // Fetch lowercase value.
-        var value = this.value.toLowerCase();
-
-        // Display length of inputted text.
-        document.getElementById('length').innerHTML = value.length;
-
-        // Reset characters.
-        var characters = '0123456789abcdefghijklmnopqrstuvwxyz !@#$%^&*()_-+={}|:;"\'<,>.?/'.split('');
-        var output = '';
-
-        // Count how many times each character appears in inputted text.
-        for(var character in characters){
-            var character_count = value.replace(
-              new RegExp(
-                '[^' + characters[character] + ']',
-                'g'// Global
-              ),
-              ''
-            ).length;
-
-            // Only display characters that appear in inputted text.
-            if(character_count <= 0){
-                continue;
-            }
-
-            output +=
-              '<tr><td>'
-              + characters[character]
-              + '</td><td>'
-              + character_count
-              + '</td><td>'
-              + ((character_count / value.length) * 100).toFixed(7)
-              + '%</td></tr>';
-        }
-
-        core_html_modify({
-          'id': 'characters',
-          'properties': {
-            'innerHTML': output,
-          },
-        });
-    };
 }
