820659817f1e35da0e2164abfcd237dc2451db14
Jan Hořava <czechinthemail@gmail.com>
2025-05-05 23:08:26 -0700

    changed all keycode to code

--- repo.js
+++ repo.js
@@ -6,31 +6,31 @@ function fetch_keyinfo(key){
     core_elements['key-single'].innerHTML =
       '<span class=medium>'
         + key
-        + '</span><br>KeyCode: '
+        + '</span><br>Code: '
         + key.charCodeAt(0);
 }
 
-function fetch_keyinfo_keycode(code){
+function fetch_keyinfo_code(code){
     fetch_keyinfo(String.fromCharCode(code));
 }
 
 function generate_list(){
     core_storage_save();
 
-    let loop_counter = core_storage_data['keycode-range'] - 1;
+    let loop_counter = core_storage_data['code-range'] - 1;
     if(loop_counter < 0){
         return;
     }
 
     const keys = [];
     do{
-        const code = core_storage_data['base-keycode'] + loop_counter;
+        const code = core_storage_data['base-code'] + loop_counter;
         const char = String.fromCharCode(code);
 
         keys.splice(
           0,
           0,
-          '<a href="javascript:fetch_keyinfo_keycode(' + code + ');" style="border:1px solid #aaa;display:inline-block;height:1em;text-decoration:none;width:25px">' + char + '</a>'
+          '<a href="javascript:fetch_keyinfo_code(' + code + ');" style="border:1px solid #aaa;display:inline-block;height:1em;text-decoration:none;width:25px">' + char + '</a>'
         );
     }while(loop_counter--);
 
@@ -45,9 +45,9 @@ function repo_init(){
               fetch_keyinfo(core_elements['key'].value);
           },
         },
-        'fetch-keyinfo-keycode': {
+        'fetch-keyinfo-code': {
           'onclick': function(){
-              fetch_keyinfo_keycode(core_elements['keycode'].value);
+              fetch_keyinfo_code(core_elements['code'].value);
           },
         },
         'generate': {
@@ -89,16 +89,16 @@ function repo_init(){
         },
       },
       'storage': {
-        'base-keycode': 0,
+        'base-code': 0,
+        'code': 72,
+        'code-range': 100,
         'key': 'H',
-        'keycode': 72,
-        'keycode-range': 100,
       },
       'title': 'TextInfo.htm',
       'ui-elements': [
         'characters',
+        'code',
         'key',
-        'keycode',
         'key-list',
         'key-single',
         'length',
