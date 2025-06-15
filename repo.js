b3c713425c42d6c81c9204d2d4074000c9ba616b
Jan Hořava <czechinthemail@gmail.com>
2025-06-15 11:46:59 -0700

    simplified property usage

--- repo.js
+++ repo.js
@@ -3,7 +3,7 @@
 function fetch_keyinfo(key){
     core_storage_save();
 
-    core_elements['key-single'].innerHTML =
+    core_elements.key_single.innerHTML =
       '<span class=medium>'
         + key
         + '</span><br>Code: '
@@ -17,14 +17,14 @@ function fetch_keyinfo_code(code){
 function generate_list(){
     core_storage_save();
 
-    let loop_counter = core_storage_data['code-range'] - 1;
+    let loop_counter = core_storage_data.code_range - 1;
     if(loop_counter < 0){
         return;
     }
 
     const keys = [];
     do{
-        const code = core_storage_data['base-code'] + loop_counter;
+        const code = core_storage_data.base_code + loop_counter;
         const char = String.fromCharCode(code);
 
         keys.splice(
@@ -34,20 +34,20 @@ function generate_list(){
         );
     }while(loop_counter--);
 
-    core_elements['key-list'].innerHTML = keys.join(' ');
+    core_elements.key_list.innerHTML = keys.join(' ');
 }
 
 function repo_init(){
     core_repo_init({
       'events': {
-        'fetch-keyinfo-key': {
+        'fetch_keyinfo_key': {
           'onclick': function(){
-              fetch_keyinfo(core_elements['key'].value);
+              fetch_keyinfo(core_elements.key.value);
           },
         },
-        'fetch-keyinfo-code': {
+        'fetch_keyinfo_code': {
           'onclick': function(){
-              fetch_keyinfo_code(core_elements['code'].value);
+              fetch_keyinfo_code(core_elements.code.value);
           },
         },
         'generate': {
@@ -56,8 +56,8 @@ function repo_init(){
         'text': {
           'oninput': function(){
               const value = this.value.toLowerCase();
-              core_elements['length'].textContent = value.length;
-              core_elements['lines'].textContent = value.split(/\n/).length;
+              core_elements.length.textContent = value.length;
+              core_elements.lines.textContent = value.split(/\n/).length;
 
               const characters = '0123456789abcdefghijklmnopqrstuvwxyz !@#$%^&*()_-+={}|:;"\'<,>.?/'.split('');
               let output = '';
@@ -84,14 +84,14 @@ function repo_init(){
                     + '%</td></tr>';
               }
 
-              core_elements['characters'].innerHTML = output;
+              core_elements.characters.innerHTML = output;
           },
         },
       },
       'storage': {
-        'base-code': 0,
+        'base_code': 0,
         'code': 72,
-        'code-range': 100,
+        'code_range': 100,
         'key': 'H',
       },
       'title': 'TextInfo.htm',
@@ -99,8 +99,8 @@ function repo_init(){
         'characters',
         'code',
         'key',
-        'key-list',
-        'key-single',
+        'key_list',
+        'key_single',
         'length',
         'lines',
       ],
